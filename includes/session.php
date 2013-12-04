9df3a7d8b40e5cbed660ad5131a33ddb7761306f
Nigel Osborne <kiwi3685@gmail.com>
2013-12-05 10:03:07 +1300

    Next version is 1.4.4.2

--- includes/session.php
+++ includes/session.php
@@ -31,8 +31,8 @@ if (!defined('WT_SCRIPT_NAME')) {
 
 // Identify ourself
 define('WT_WEBTREES',        'kiwi-webtrees');
-define('WT_VERSION',         '1.4.4.1');
-define('WT_VERSION_RELEASE', ''); // “dev”, “beta”, “rc1”, “”, etc.
+define('WT_VERSION',         '1.4.4.2');
+define('WT_VERSION_RELEASE', 'dev'); // “dev”, “beta”, “rc1”, “”, etc.
 define('WT_VERSION_TEXT',    trim(WT_VERSION.' '.WT_VERSION_RELEASE));
 
 // External URLs
