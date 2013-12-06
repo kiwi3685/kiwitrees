7c3e236160f57febb1cafd219f1fb0ae35e2d999
Nigel Osborne <kiwi3685@gmail.com>
2013-12-07 11:43:46 +1300

    status messages do not show up in pop-up frames on the xenea theme.

--- themes/xenea/header.php
+++ themes/xenea/header.php
@@ -171,6 +171,8 @@ if ($view!='simple') { // Use "simple" headers for popup windows
 	echo
 		'</ul>',  // <ul id="main-menu">
 		'</div>', // <div id="topMenu">
-		WT_FlashMessages::getHtmlMessages(); // Feedback from asynchronous actions
 }
-echo $javascript, '<div id="content">';
+echo
+        $javascript,
+        WT_FlashMessages::getHtmlMessages(), // Feedback from asynchronous actions
+        '<div id="content">';
