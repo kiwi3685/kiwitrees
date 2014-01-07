292aa615dd96f4dfb035183b8cf8d9992530fceb
Nigel Osborne <kiwi3685@gmail.com>
2014-01-08 10:25:55 +1300

    Update copyright dates

--- individual.php
+++ individual.php
@@ -7,7 +7,7 @@
 // Copyright (C) 2014 webtrees development team.
 //
 // Derived from PhpGedView
-Copyright (C) 2002 to 2010  PGV Development Team
+// Copyright (C) 2002 to 2010  PGV Development Team
 //
 // Sidebar controls courtesy of http://devheart.org/articles/jquery-collapsible-sidebar-layout/
 //
