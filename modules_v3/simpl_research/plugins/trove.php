b9afa9866e30dfcc8023e5f2a34d069c36186e61
kiwi3685 <Kiwi3685@gmail.com>
2014-04-22 16:12:38 +1200

    Add Australian Trove site to simpl_research

---
+++ modules_v3/simpl_research/plugins/trove.php
@@ -0,0 +1,20 @@
+<?php
+
+if (!defined('WT_WEBTREES')) {
+	header('HTTP/1.0 403 Forbidden');
+	exit;
+}
+
+class trove_plugin extends research_base_plugin {
+	static function getName() {
+		return 'Trove (Australia)';
+	}	
+	
+	static function create_link($fullname, $givn, $first, $middle, $prefix, $surn, $surname) {
+		return $link = 'http://trove.nla.gov.au/result?q=+%22'.str_replace(" ", "+", $fullname).'%22&l-australian=y';
+	}
+
+	static function create_sublink($fullname, $givn, $first, $middle, $prefix, $surn, $surname) {
+		return false;
+	}
+}
