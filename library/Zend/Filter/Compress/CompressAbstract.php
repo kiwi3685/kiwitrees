bbc60f0103f8002894ac12d9cf264354a914ea9a
kiwi3685 <Kiwi3685@gmail.com>
2014-05-28 17:01:06 +1200

    Update Zend

--- library/Zend/Filter/Compress/CompressAbstract.php
+++ library/Zend/Filter/Compress/CompressAbstract.php
@@ -14,22 +14,22 @@
  *
  * @category   Zend
  * @package    Zend_Filter
- * @copyright  Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
+ * @copyright  Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
- * @version    $Id: CompressAbstract.php 24593 2012-01-05 20:35:02Z matthew $
+ * @version    $Id$
  */
 
 /**
  * @see Zend_Filter_Compress_CompressInterface
  */
-require_once 'Zend/Filter/Compress/CompressInterface.php';
+// require_once 'Zend/Filter/Compress/CompressInterface.php';
 
 /**
  * Abstract compression adapter
  *
  * @category   Zend
  * @package    Zend_Filter
- * @copyright  Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
+ * @copyright  Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
 abstract class Zend_Filter_Compress_CompressAbstract implements Zend_Filter_Compress_CompressInterface
