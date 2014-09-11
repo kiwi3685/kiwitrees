b9905370f7f41736e847aa47dc20d587491a34a9
kiwi3685 <Kiwi3685@gmail.com>
2014-09-11 16:06:45 +1200

    Change order of family tree admin options

--- themes/_administration/header.php
+++ themes/_administration/header.php
@@ -152,16 +152,16 @@ echo '		</div>
 
 			<h3 id="tree-tools"><i class="fa fa-wrench fa-fw"></i>', WT_I18N::translate('Family tree tools'), '</h3>
 			<div>
-				<p><a href="index_edit.php?gedcom_id=-1" onclick="return modalDialog(\'index_edit.php?gedcom_id=-1'.'\', \'',  WT_I18N::translate('Set the default blocks for new family trees'), '\');">', WT_I18N::translate('Set the default blocks'), '</a></p>
 				<p><a ', (WT_SCRIPT_NAME=="admin_trees_check.php"		? 'class="current" ' : ''), 'href="admin_trees_check.php">',		WT_I18N::translate('Check for errors'			), '</a></p>
-				<p><a ', (WT_SCRIPT_NAME=="admin_site_merge.php"  		? 'class="current" ' : ''), 'href="admin_site_merge.php">',			WT_I18N::translate('Merge records'				), '</a></p>
-				<p><a ', (WT_SCRIPT_NAME=="admin_site_other.php"    	? 'class="current" ' : ''), 'href="admin_site_other.php">',			WT_I18N::translate('Add unlinked records'		), '</a></p>
 				<p><a ', (WT_SCRIPT_NAME=="admin_site_change.php"    	? 'class="current" ' : ''), 'href="admin_site_change.php">',		WT_I18N::translate('Changes log'				), '</a></p>
-				<p><a ', (WT_SCRIPT_NAME=="admin_trees_renumber.php" 	? 'class="current" ' : ''), 'href="admin_trees_renumber.php">',		WT_I18N::translate('Renumber family tree'		), '</a></p>
-				<p><a ', (WT_SCRIPT_NAME=="admin_trees_append.php"    	? 'class="current" ' : ''), 'href="admin_trees_append.php">',		WT_I18N::translate('newt_listbox_append_entry(listbox, text, data) trees'				), '</a></p>
+				<p><a ', (WT_SCRIPT_NAME=="admin_site_other.php"    	? 'class="current" ' : ''), 'href="admin_site_other.php">',			WT_I18N::translate('Add unlinked records'		), '</a></p>
 				<p><a ', (WT_SCRIPT_NAME=="admin_trees_places.php"   	? 'class="current" ' : ''), 'href="admin_trees_places.php">',		WT_I18N::translate('Update place names'			), '</a></p>
+				<p><a ', (WT_SCRIPT_NAME=="admin_site_merge.php"  		? 'class="current" ' : ''), 'href="admin_site_merge.php">',			WT_I18N::translate('Merge records'				), '</a></p>
+				<p><a ', (WT_SCRIPT_NAME=="admin_trees_renumber.php" 	? 'class="current" ' : ''), 'href="admin_trees_renumber.php">',		WT_I18N::translate('Renumber family tree'		), '</a></p>
+				<p><a ', (WT_SCRIPT_NAME=="admin_trees_append.php"    	? 'class="current" ' : ''), 'href="admin_trees_append.php">',		WT_I18N::translate('Append family tree'			), '</a></p>
 				<p><a ', (WT_SCRIPT_NAME=="admin_trees_duplicates.php"  ? 'class="current" ' : ''), 'href="admin_trees_duplicates.php">',	WT_I18N::translate('Find duplicate individuals'	), '</a></p>
 				<p><a ', (WT_SCRIPT_NAME=="admin_trees_unlinked.php"	? 'class="current" ' : ''), 'href="admin_trees_unlinked.php">',		WT_I18N::translate('Find unlinked individuals'	), '</a></p>
+				<p><a href="index_edit.php?gedcom_id=-1" onclick="return modalDialog(\'index_edit.php?gedcom_id=-1'.'\', \'',  WT_I18N::translate('Set the default blocks for new family trees'), '\');">', WT_I18N::translate('Set the default blocks'), '</a></p>
 			</div>';
 
 if (WT_USER_IS_ADMIN) {
