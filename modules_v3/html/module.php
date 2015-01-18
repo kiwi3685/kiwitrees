581c43ef97a867f724207f537f99481fb25e0f47
kiwi3685 <Kiwi3685@gmail.com>
2015-01-19 09:37:16 +1300

    Add widget version of HTML block

--- modules_v3/html/module.php
+++ modules_v3/html/module.php
@@ -126,7 +126,7 @@ class html_WT_Module extends WT_Module implements WT_Module_Block {
 
 	// Implement class WT_Module_Block
 	public function isUserBlock() {
-		return true;
+		return false;
 	}
 
 	// Implement class WT_Module_Block
