19b7713207cf2ea4930a46a2b24c7add1b759b66
kiwi3685 <Kiwi3685@gmail.com>
2015-01-22 09:56:09 +1300

    Remove My page option from upcoming events module

--- modules_v3/upcoming_events/module.php
+++ modules_v3/upcoming_events/module.php
@@ -102,7 +102,7 @@ class upcoming_events_WT_Module extends WT_Module implements WT_Module_Block {
 
 	// Implement class WT_Module_Block
 	public function isUserBlock() {
-		return true;
+		return false;
 	}
 
 	// Implement class WT_Module_Block
