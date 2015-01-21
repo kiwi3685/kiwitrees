6db239f773d657db4e9a0a3e6ec2c750bf4e468f
kiwi3685 <Kiwi3685@gmail.com>
2015-01-22 10:35:45 +1300

    Tidy up widget bar code

--- widget-bar.php
+++ widget-bar.php
@@ -25,41 +25,26 @@
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-//define('WT_SCRIPT_NAME', 'widget-bar.php');
-//require './includes/session.php';
-
 global $controller, $ctype;
 
 //-- get the widgets list
 $widgets = WT_Module::getActiveWidgets(WT_GED_ID, WT_PRIV_HIDE);
 
-echo '
-	<div id="widget-bar">';
-		foreach ($widgets as $module_name=>$module) {
-			$class_name = $module_name.'_WT_Module';
-			$module = new $class_name;
-			$widget = WT_DB::prepare(
-				"SELECT SQL_CACHE * FROM `##block` WHERE module_name = ?"
-			)->execute(array($module_name))->fetchOneRow();
-			if (!$widget) {
-				WT_DB::prepare("INSERT INTO `##block` (module_name) VALUES (?)")
-					->execute(array($module_name));
-				$widget = WT_DB::prepare("SELECT SQL_CACHE * FROM `##block` WHERE module_name = ?")
-					->execute(array($module_name))->fetchOneRow();
-			}
-
-			echo '<div class="widget">';
-				if ($SEARCH_SPIDER || !$module->loadAjax()) {
-					// Load the widget directly
-					$module->getWidget($widget->block_id);
-				} else {
-					// Load the widget asynchronously
-					echo '<div id="', $module_name, '"><div class="loading-image">&nbsp;</div></div>';
-					$controller->addInlineJavascript(
-						'jQuery("#'.$module_name.'").load("index.php?ctype='.$ctype.'&action=ajax&module_name='.$module_name.'");'
-					);
-				}
-			echo '</div>';
+echo '<div id="widget-bar">';
+	foreach ($widgets as $module_name=>$module) {
+		$class_name = $module_name.'_WT_Module';
+		$module = new $class_name;
+		$widget = WT_DB::prepare(
+			"SELECT SQL_CACHE * FROM `##block` WHERE module_name = ?"
+		)->execute(array($module_name))->fetchOneRow();
+		if (!$widget) {
+			WT_DB::prepare("INSERT INTO `##block` (module_name) VALUES (?)")
+				->execute(array($module_name));
+			$widget = WT_DB::prepare("SELECT SQL_CACHE * FROM `##block` WHERE module_name = ?")
+				->execute(array($module_name))->fetchOneRow();
 		}
+
+		echo '<div class="widget">' , $module->getWidget($widget->block_id) , '</div>';
+	}
 echo '</div>';
 	
