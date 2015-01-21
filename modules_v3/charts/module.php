904e0d9e9be6fc214d871464d2be10a414be7284
kiwi3685 <Kiwi3685@gmail.com>
2015-01-22 10:01:13 +1300

    Remove My page option from charts and login modules

--- modules_v3/charts/module.php
+++ modules_v3/charts/module.php
@@ -173,7 +173,7 @@ class charts_WT_Module extends WT_Module implements WT_Module_Block {
 
 	// Implement class WT_Module_Block
 	public function isUserBlock() {
-		return true;
+		return false;
 	}
 
 	// Implement class WT_Module_Block
