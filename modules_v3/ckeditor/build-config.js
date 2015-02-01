dc983b906a62ca187aea278a0dfcc7301353166f
kiwi3685 <Kiwi3685@gmail.com>
2015-02-01 16:37:57 +1300

    Update CKEditor to 4.4.7

--- modules_v3/ckeditor/build-config.js
+++ modules_v3/ckeditor/build-config.js
@@ -1,5 +1,5 @@
 ﻿/**
- * @license Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
+ * @license Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
@@ -13,10 +13,10 @@
  * (1) http://ckeditor.com/builder
  *     Visit online builder to build CKEditor from scratch.
  *
- * (2) http://ckeditor.com/builder/d7ec63175165fdc0710d4cf9544ce14a
+ * (2) http://ckeditor.com/builder/a438015e56d416ce02e869293a7f2072
  *     Visit online builder to build CKEditor, starting with the same setup as before.
  *
- * (3) http://ckeditor.com/builder/download/d7ec63175165fdc0710d4cf9544ce14a
+ * (3) http://ckeditor.com/builder/download/a438015e56d416ce02e869293a7f2072
  *     Straight download link to the latest version of CKEditor (Optimized) with the same setup as before.
  *
  * NOTE:
@@ -34,6 +34,7 @@ var CKBUILDER_CONFIG = {
 		'bender-out.log',
 		'dev',
 		'.DS_Store',
+		'.editorconfig',
 		'.gitattributes',
 		'.gitignore',
 		'gruntfile.js',
