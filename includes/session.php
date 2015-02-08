0cdc314a1670c2f628ba16cf015b1f63653d355c
kiwi3685 <Kiwi3685@gmail.com>
2015-02-08 16:51:30 +1300

    Cirrect capitalisation of file name

--- includes/session.php
+++ includes/session.php
@@ -56,7 +56,7 @@ if (WT_USE_GOOGLE_API) {
 }
 define('WT_JQUERY_COLORBOX_URL',   WT_STATIC_URL.'js/jquery.colorbox-min.js');		// 1.5.14
 define('WT_JQUERY_COOKIE_URL',     WT_STATIC_URL.'js/jquery.cookie.js');			// 1.4.1
-define('WT_JQUERY_DATATABLES_URL', WT_STATIC_URL.'js/jquery.datatables.min.js');	// 1.10.4
+define('WT_JQUERY_DATATABLES_URL', WT_STATIC_URL.'js/jquery.dataTables.min.js');	// 1.10.4
 define('WT_JQUERY_JEDITABLE_URL',  WT_STATIC_URL.'js/jquery.jeditable.js');			// 1.7.3
 define('WT_JQUERY_WHEELZOOM_URL',  WT_STATIC_URL.'js/jquery.wheelzoom.js');			// 2.0.0
 define('WT_MODERNIZR_URL',         WT_STATIC_URL.'js/modernizr.custom.js');			// 2.6.2
