6b7778b3e8c8591e1523726d1a453734d9bb651f
kiwi3685 <Kiwi3685@gmail.com>
2015-05-02 17:16:19 +1200

    Further changes to Update place names

--- admin_trees_places.php
+++ admin_trees_places.php
@@ -100,16 +100,16 @@ $controller
 		<?php echo WT_I18N::translate('This will update the highest-level part or parts of the place name.  For example, “Mexico” will match “Quintana Roo, Mexico”, but not “Santa Fe, New Mexico”.'); ?>
 	</p>
 	<form method="post">
-		<dl>
-			<dt><?php echo WT_I18N::translate('Family tree'); ?></dt>
-			<dd><?php echo select_edit_control('ged', WT_Tree::getNameList(), null, WT_GEDCOM, 'autofocus'); ?></dd>
-			<dt><label for="search"><?php echo WT_I18N::translate('Search for'); ?></label></dt>
-			<dd><input name="search" id="search" type="text" data-autocomplete-type="PLAC" value="<?= WT_Filter::escapeHtml($search) ?>" required></dd>
-			<dt><label for="replace"><?php echo WT_I18N::translate('Replace with'); ?></label></dt>
-			<dd><input name="replace" id="replace" type="text" value="<?= WT_Filter::escapeHtml($replace) ?>" required></dd>
-		</dl>
-		<button type="submit" value="preview"><?php echo /* I18N: button label */ WT_I18N::translate('preview'); ?></button>
-		<button type="submit" value="update" name="confirm"><?php echo /* I18N: button label */ WT_I18N::translate('update'); ?></button>
+		<label for "ged"><?php echo WT_I18N::translate('Family tree'); ?></label>
+		<?php echo select_edit_control('ged', WT_Tree::getNameList(), null, WT_GEDCOM, 'autofocus'); ?>
+		<label for="search"><?php echo WT_I18N::translate('Search for'); ?></label>
+		<input name="search" id="search" type="text" data-autocomplete-type="PLAC" value="<?= WT_Filter::escapeHtml($search) ?>" required>
+		<label for="replace"><?php echo WT_I18N::translate('Replace with'); ?></label>
+		<input name="replace" id="replace" type="text" value="<?php WT_Filter::escapeHtml($replace) ?>" required>
+		<p>
+			<button type="submit" value="preview"><?php echo /* I18N: button label */ WT_I18N::translate('preview'); ?></button>
+			<button type="submit" value="update" name="confirm"><?php echo /* I18N: button label */ WT_I18N::translate('update'); ?></button>
+		</p>
 	</form>
 
 	<p class="error">
@@ -125,7 +125,7 @@ $controller
 			<?php foreach ($changes as $old_place => $new_place) { ?>
 			<li>
 				<?php echo WT_Filter::escapeHtml($old_place); ?>
-				&rarr;
+				&nbsp;&rarr;&nbsp;
 				<?php echo WT_Filter::escapeHtml($new_place); ?>
 			</li>
 			<?php } ?>
