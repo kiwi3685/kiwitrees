30135049912a1274559e553488c6af129b5d833f
kiwi3685 <Kiwi3685@gmail.com>
2015-05-05 11:54:11 +1200

    remove unused code

--- widget-bar.php
+++ widget-bar.php
@@ -22,8 +22,6 @@
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-global $controller, $ctype;
-
 //-- get the widgets list
 $widgets = WT_Module::getActiveWidgets(WT_GED_ID, WT_PRIV_HIDE);
 
