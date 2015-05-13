18ede51ee6b5b57f5f84d79191e543f660a651dc
kiwi3685 <Kiwi3685@gmail.com>
2015-05-14 10:44:13 +1200

    Update comment on datatables upgrade

--- includes/session.php
+++ includes/session.php
@@ -55,7 +55,7 @@ if (WT_USE_GOOGLE_API) {
 	define('WT_JQUERYUI_URL',		WT_STATIC_URL .'js/jquery-ui.js');				// 1.10.4
 }
 define('WT_JQUERY_COLORBOX_URL',	WT_STATIC_URL . 'js/jquery.colorbox-min.js');	// 1.6.0
-define('WT_JQUERY_DATATABLES_URL',	WT_STATIC_URL . 'js/jquery.dataTables.min.js');	// 1.10.5 24-03-2015
+define('WT_JQUERY_DATATABLES_URL',	WT_STATIC_URL . 'js/jquery.dataTables.min.js');	// 1.10.7 30-04-2015
 define('WT_JQUERY_JEDITABLE_URL',	WT_STATIC_URL . 'js/jquery.jeditable.js');		// 1.7.3
 define('WT_JQUERY_WHEELZOOM_URL',	WT_STATIC_URL . 'js/jquery.wheelzoom.js');		// 2.0.0
 define('WT_MODERNIZR_URL',			WT_STATIC_URL . 'js/modernizr.custom.js');		// 2.6.2
