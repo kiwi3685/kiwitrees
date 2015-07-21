6e41b334fe5d728281bc9686c44794968501f254
kiwi3685 <Kiwi3685@gmail.com>
2015-07-21 17:55:38 +1200

    Update ver number of 3.2 next version

--- includes/session.php
+++ includes/session.php
@@ -32,7 +32,7 @@ if (!defined('WT_SCRIPT_NAME')) {
 
 // Identify ourself
 define('WT_WEBTREES',        'Kiwitrees');
-define('WT_VERSION',         '3.1.0');
+define('WT_VERSION',         '3.2.0');
 define('WT_VERSION_RELEASE', ''); // “dev”, “beta”, “rc1”, “”, etc.
 define('WT_VERSION_TEXT',    trim(WT_VERSION.' '.WT_VERSION_RELEASE));
 
