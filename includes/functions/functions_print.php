08facaab78632cad6d9d6757df42eef82eff9778
kiwi3685 <Kiwi3685@gmail.com>
2015-07-23 11:41:07 +1200

    White space

--- includes/functions/functions_print.php
+++ includes/functions/functions_print.php
@@ -1120,7 +1120,7 @@ function print_add_new_fact($id, $usedfacts, $type) {
 	echo '<span class="quickfacts">';
 		foreach ($quickfacts as $fact) {
 			echo '<a href="edit_interface.php?action=add&pid=' . $id . '&fact=' . $fact . '&accesstime=' . WT_TIMESTAMP . '&ged=' . WT_GEDCOM . '" target="_blank">', WT_Gedcom_Tag::getLabel($fact), '</a>';
-			}
+		}
 	echo '</span></form>';
 	echo '</td></tr>';
 }
