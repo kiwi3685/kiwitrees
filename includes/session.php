e9c645285b1797651304bd68af927d70c36e365e
kiwi3685 <Kiwi3685@gmail.com>
2015-08-24 09:48:16 +1200

    Prepare for next release - 3.1.2

--- includes/session.php
+++ includes/session.php
@@ -32,7 +32,7 @@ if (!defined('WT_SCRIPT_NAME')) {
 
 // Identify ourself
 define('WT_WEBTREES',        'Kiwitrees');
-define('WT_VERSION',         '3.1.1');
+define('WT_VERSION',         '3.1.2');
 define('WT_VERSION_RELEASE', ''); // “dev”, “beta”, “rc1”, “”, etc.
 define('WT_VERSION_TEXT',    trim(WT_VERSION.' '.WT_VERSION_RELEASE));
 
