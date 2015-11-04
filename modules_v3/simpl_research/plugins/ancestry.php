665aa4776ec416991d920845f6fc32186d7965da
kiwi3685 <Kiwi3685@gmail.com>
2015-11-05 09:15:43 +1300

    Error in ancestry plugin code

--- modules_v3/simpl_research/plugins/ancestry.php
+++ modules_v3/simpl_research/plugins/ancestry.php
@@ -11,7 +11,7 @@ class ancestry_plugin extends research_base_plugin {
 	}
 
 	static function create_link($fullname, $givn, $first, $middle, $prefix, $surn, $surname, $birth_year) {
-		return $link = '#'
+		return $link = '#';
 	}
 
 	static function create_sublink($fullname, $givn, $first, $middle, $prefix, $surn, $surname, $birth_year) {
