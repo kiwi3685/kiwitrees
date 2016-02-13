53e2b5a9cabb3b538ae08d4a14193c40836c2be9
kiwi3685 <Kiwi3685@gmail.com>
2016-02-13 18:37:01 +1300

    Add fixed footer to all colors themes

--- themes/colors/footer.php
+++ themes/colors/footer.php
@@ -35,11 +35,9 @@ echo '</div>'; // <div id="content">
 
 if ($view!='simple') {
 	echo '
-		<div id="footer" class="', $TEXT_DIRECTION, '">
-			<br>';
+		<div id="footer" class="', $TEXT_DIRECTION, '">';
 				if (contact_links() != '' && !array_key_exists('contact', WT_Module::getActiveModules())) echo contact_links();
 	echo '
-			<br>
 			<p class="logo">',
 				WT_I18N::translate('Powered by '), '
 				<a href="', WT_WEBTREES_URL, '" target="_blank" title="', WT_WEBTREES, ' ', WT_VERSION_TEXT, '">', WT_WEBTREES,'<span>&trade;</span></a>
