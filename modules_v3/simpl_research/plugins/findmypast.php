cbbcf93b0ede934844b509ee1b1a1f24e5f11fd8
kiwi3685 <Kiwi3685@gmail.com>
2016-03-11 08:48:19 +1300

    Add birth year to FMP research plugin

--- modules_v3/simpl_research/plugins/findmypast.php
+++ modules_v3/simpl_research/plugins/findmypast.php
@@ -10,15 +10,14 @@ class findmypast_plugin extends research_base_plugin {
 		return 'findmypast';
 	}
 
-	static function create_link($fullname, $givn, $first, $middle, $prefix, $surn, $surname) {
-		return $link = 'http://search.findmypast.com/search/world-records?firstname=' . $givn . '&firstname_variants=true&lastname=' . $surname;
+	static function create_link($fullname, $givn, $first, $middle, $prefix, $surn, $surname, $birth_year) {
+		return $link = 'http://search.findmypast.com/search/world-records?firstname=' . $givn . '&firstname_variants=true&lastname=' . $surname . '&yearofbirth=' . $birth_year . '&yearofbirth_offset=2';
 	}
-
 	static function create_sublink($fullname, $givn, $first, $middle, $prefix, $surn, $surname) {
 		return false;
 	}
-	
+
 	static function encode_plus() {
-		return false;	
+		return false;
 	}
 }
