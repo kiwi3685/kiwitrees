fa65271db0cbb039213a6d2607bc26a6b966ffa2
kiwi3685 <Kiwi3685@gmail.com>
2016-03-10 16:48:44 +1300

    Fix minor bug - doubled age display on UK Register check

--- modules_v3/uk_register/module.php
+++ modules_v3/uk_register/module.php
@@ -264,7 +264,7 @@ class uk_register_WT_Module extends WT_Module implements WT_Module_Resources {
 							if (stripos($bef_plac, $data_source['place']) !== false || stripos($aft_plac, $data_source['place']) !== false) {
 								$age_at_census = substr($data_source['date'],7,4) - $indi->getBirthDate()->gregorianYear();
 								$desc_event = WT_Gedcom_Tag::getLabel($data_source['event']);
-								$missing_text .= '<li><i>' . WT_I18N::translate('Age') . ' - ' . $age_at_census . '</i></li>';
+								$missing_text = '<li><i>' . WT_I18N::translate('Age') . ' - ' . $age_at_census . '</i></li>';
 								// Person died after 1991 - make note
 								if ($indi->getEstimatedDeathDate()->gregorianYear() > '1991') {
 									$missing_text .= '<li><i>' . WT_I18N::translate('Probably redacted - living or died after 1991') . '</i></li>';
