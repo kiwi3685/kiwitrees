f4b1ae9822091ecb85074942ff953664be08d749
kiwi3685 <Kiwi3685@gmail.com>
2016-03-25 10:34:25 +1300

    New research plugin - Gelders Archief

---
+++ modules_v3/simpl_research/plugins/geldersarchief.php
@@ -0,0 +1,24 @@
+<?php
+
+if (!defined('WT_WEBTREES')) {
+	header('HTTP/1.0 403 Forbidden');
+	exit;
+}
+
+class geldersarchief_plugin extends research_base_plugin {
+	static function getName() {
+		return 'Gelders Archief';
+	}
+
+	static function create_link($fullname, $givn, $first, $middle, $prefix, $surn, $surname) {
+		return $link = 	'http://www.geldersarchief.nl/zoeken/?mizk_alle=' . $givn . '+' . $surn . '&mizig=128&miview=tbl';
+	}
+
+	static function create_sublink($fullname, $givn, $first, $middle, $prefix, $surn, $surname) {
+		return false;
+	}
+
+	static function encode_plus() {
+		return false;
+	}
+}
