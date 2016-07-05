3321698a452a6c9a4fdd5eac7e61568a94d04fe0
kiwi3685 <Kiwi3685@gmail.com>
2016-07-06 07:51:42 +1200

    Add special character link to Update Places module

--- admin_trees_places.php
+++ admin_trees_places.php
@@ -102,9 +102,9 @@ $controller
 		<label for "ged"><?php echo WT_I18N::translate('Family tree'); ?></label>
 		<?php echo select_edit_control('ged', WT_Tree::getNameList(), null, WT_GEDCOM, 'autofocus'); ?>
 		<label for="search"><?php echo WT_I18N::translate('Search for'); ?></label>
-		<input name="search" id="search" type="text" data-autocomplete-type="PLAC" value="<?php echo WT_Filter::escapeHtml($search); ?>" required>
+		<input name="search" id="search" type="text" data-autocomplete-type="PLAC" value="<?php echo WT_Filter::escapeHtml($search); ?>" required><?php echo print_specialchar_link('search'); ?>
 		<label for="replace"><?php echo WT_I18N::translate('Replace with'); ?></label>
-		<input name="replace" id="replace" type="text" data-autocomplete-type="PLAC" value="<?php echo WT_Filter::escapeHtml($replace); ?>" required>
+		<input name="replace" id="replace" type="text" data-autocomplete-type="PLAC" value="<?php echo WT_Filter::escapeHtml($replace); ?>" required><?php echo print_specialchar_link('replace'); ?>
 		<p>
 			<button type="submit" value="preview"><?php echo /* I18N: button label */ WT_I18N::translate('preview'); ?></button>
 			<button type="submit" value="update" name="confirm"><?php echo /* I18N: button label */ WT_I18N::translate('update'); ?></button>
