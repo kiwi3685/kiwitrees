106eccdc7db6ff1addf3dda6809f5852094c4572
kiwi3685 <Kiwi3685@gmail.com>
2017-02-22 18:02:19 +1300

    Correction to research plugin

--- modules_v3/simpl_research/plugins/paperspast.php
+++ modules_v3/simpl_research/plugins/paperspast.php
@@ -19,7 +19,9 @@ class paperspast_plugin extends research_base_plugin {
 	}
 
 	static function create_link($fullname, $givn, $first, $middle, $prefix, $surn, $surname, $birth_year, $death_year, $gender) {
-		return $link = 'https://paperspast.natlib.govt.nz/newspapers?phrase=2&query=+' . $first  . '+' . $surname . '&start_date=' . $birth_year . '-01-01&end_date=' . $death_year . '-12-31';
+		$birth_year ? $birth_year = $birth_year . '-01-01' : $birth_year = '';
+		$death_year ? $death_year = $death_year . '-12-31' : $death_year = '';
+		return $link = 'https://paperspast.natlib.govt.nz/newspapers?phrase=2&query=+' . $first  . '+' . $surname . '&start_date=' . $birth_year . '&end_date=' . $death_year;
 	}
 
 	static function create_sublink($fullname, $givn, $first, $middle, $prefix, $surn, $surname, $birth_year, $death_year, $gender) {
