9ce1328a40fffffad37b1eb91342f003b5ec0b62
kiwi3685 <Kiwi3685@gmail.com>
2017-05-09 10:49:30 +1200

    New research plugin (nz)

---
+++ modules_v4/research_links/plugins/nzgazette.php
@@ -0,0 +1,40 @@
+<?php
+
+if (!defined('WT_KIWITREES')) {
+	header('HTTP/1.0 403 Forbidden');
+	exit;
+}
+
+class nzgazette_plugin extends research_base_plugin {
+	static function getName() {
+		return 'New Zealand Gazette';
+	}
+
+	static function getPaySymbol() {
+		return false;
+	}
+
+	static function getSearchArea() {
+		return 'NZL';
+	}
+
+	static function create_link($fullname, $givn, $first, $middle, $prefix, $surn, $surname, $birth_year, $death_year, $gender) {
+		return $link = 'https://www.gazette.govt.nz/home/NoticeSearch?keyword=' . $first . '+' . $middle . '+' . $surname;
+	}
+
+	static function create_sublink($fullname, $givn, $first, $middle, $prefix, $surn, $surname, $birth_year, $death_year, $gender) {
+		return false;
+	}
+
+	static function createLinkOnly() {
+		return false;
+	}
+
+	static function createSubLinksOnly() {
+		return false;
+	}
+
+	static function encode_plus() {
+		return false;
+	}
+}
