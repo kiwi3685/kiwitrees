126fe9ea58fe4bc07be223272035456703f7a249
kiwi3685 <Kiwi3685@gmail.com>
2017-06-15 10:56:19 +1200

    Research plugin - add limits to FreeBMD date ranges

--- modules_v4/research_links/plugins/freebmd.php
+++ modules_v4/research_links/plugins/freebmd.php
@@ -21,13 +21,15 @@ class freebmd_plugin extends research_base_plugin {
 	static function create_link($fullname, $givn, $first, $middle, $prefix, $surn, $surname, $birth_year, $death_year, $gender) {
 		// This is a post form, so it will be sent with Javascript
 		$birth_year == '' ? $birth_year = '' : $birth_year = $birth_year - 5;
+		$birth_year = max($birth_year, 1837); // Sep qtr 1837 is earlies avaiable data
 		$death_year == '' ? $death_year = '' : $death_year = $death_year + 5;
+		$death_year = min($death_year, 1983); // Only data before 1984 is available
 		$url	 	= 'https://www.freebmd.org.uk/cgi/search.pl';
 		$params	 	= array(
 			'type'		=> 'All Types',
 			'surname'	=> $surn,
 			'given'		=> $first,
-			'sq'		=> '1',
+			'sq'		=> ($birth_year == 1837 ? '3' : '1'),
 			'start'		=> $birth_year,
 			'eq'		=> '4',
 			'end'		=> $death_year,
