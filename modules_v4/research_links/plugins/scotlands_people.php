e89f41c09c6463d0b5b9f0ce455247850e65c523
kiwi3685 <Kiwi3685@gmail.com>
2017-06-27 15:26:37 +1200

    Trap non-numeric values

--- modules_v4/research_links/plugins/scotlands_people.php
+++ modules_v4/research_links/plugins/scotlands_people.php
@@ -19,7 +19,7 @@ class scotlands_people_plugin extends research_base_plugin {
 	}
 
 	static function create_link($fullname, $givn, $first, $middle, $prefix, $surn, $surname, $birth_year, $death_year, $gender) {
-		$birth_year = $birth_year - 10;
+		$birth_year ? $birth_year = $birth_year - 10 : $birth_year = '';
 		return $link = 'https://www.scotlandspeople.gov.uk/js/search-results?search_type=People&surname=' . $surname . '&forename=' . $first . '&to_year=&from_year=' . $birth_year;
 	}
 
