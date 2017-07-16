049eb2a87e1d56af614a9a5851f5646fd1e1e706
kiwi3685 <Kiwi3685@gmail.com>
2017-07-17 08:04:32 +1200

    Fix bug in Report - Family

---
+++ library/WT/Census/CensusOfCanada1861.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Kiwitrees: Web based Family History software
+ * Copyright (C) 2012 to 2017 kiwitrees.net
+ *
+ * Derived from webtrees (www.webtrees.net)
+ * Copyright (C) 2010 to 2012 webtrees development team
+ *
+ * Derived from PhpGedView (phpgedview.sourceforge.net)
+ * Copyright (C) 2002 to 2010 PGV Development Team
+ *
+ * Kiwitrees is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with Kiwitrees.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Definitions for a census
+ */
+class WT_Census_CensusOfCanada1871 extends WT_Census_CensusOfCanada implements WT_Census_CensusInterface {
+	/**
+	 * When did this census occur.
+	 *
+	 * @return string
+	 */
+	public function censusDate() {
+		return '02 APR 1871';
+	}
+
+	/**
+	 * The columns of the census.
+	 *
+	 * @return CensusColumnInterface[]
+	 */
+	public function columns() {
+		return array(
+			new WT_Census_CensusColumnFullName($this, 'Names', 'Name of each person in family or household', 'width: 200px;'),
+			new WT_Census_CensusColumnSexMF($this, 'Sex', 'Sex (M = Male; F = Female)'),
+			new WT_Census_CensusColumnAge($this, 'Age', 'Age at nearest birthday. If under one year, give age in months'),
+			new WT_Census_CensusColumnMonthIfBornWithinYear($this, 'Born', 'Born within the last twelve months'),
+			new WT_Census_CensusColumnBirthPlaceSimple($this, 'BP', 'Country or province of birth'),
+			new WT_Census_CensusColumnReligion($this, 'Religion', 'Religion'),
+			new WT_Census_CensusColumnNationality($this, 'Origin', 'Origin (Ethnic Background)'),
+			new WT_Census_CensusColumnOccupation($this, 'Occupation', 'Profession, occupation, or trade'),
+			new WT_Census_CensusColumnConditionCan($this, 'Married', 'Whether married, or widowed'),
+			new WT_Census_CensusColumnMarriedWithinYear($this, 'MY', 'Married within last twelve months'),
+			new WT_Census_CensusColumnNull($this, 'School', 'Going to school'),
+			new WT_Census_CensusColumnNull($this, 'Read', 'over 20 unable to read'),
+			new WT_Census_CensusColumnNull($this, 'Write', 'over 20 unable to write'),
+			new WT_Census_CensusColumnNull($this, 'Infirm', 'Infirmities – (1) deaf and dumb, (2) blind, (3) unsound mind'),
+		);
+	}
+}
