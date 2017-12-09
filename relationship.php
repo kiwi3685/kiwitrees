a5937bab6e44a03428850812f614b8f990cc3257
kiwi3685 <Kiwi3685@gmail.com>
2017-12-09 16:43:13 +1300

    Replace more double spaces

--- relationship.php
+++ relationship.php
@@ -18,7 +18,7 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  * You should have received a copy of the GNU General Public License
- * along with Kiwitrees.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Kiwitrees. If not, see <http://www.gnu.org/licenses/>.
  */
 
 define('KT_SCRIPT_NAME', 'relationship.php');
