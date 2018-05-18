02adb282218ab55116c97f3632b17738cc67023b
kiwi3685 <Kiwi3685@gmail.com>
2018-05-18 13:00:56 +1200

    Add ancestry or fmp option to UK 1939 register plugin

--- modules_v4/research_links/plugins/_1939register.php
+++ modules_v4/research_links/plugins/_1939register.php
@@ -19,11 +19,22 @@ class _1939register_plugin extends research_base_plugin {
 	}
 
 	static function create_link($fullname, $givn, $first, $middle, $prefix, $surn, $surname, $birth_year, $death_year, $gender) {
-		return 'http://search.findmypast.com/results/world-records/1939-register?firstname=' . $givn . '&firstname_variants=true&lastname=' . $surn . '&lastname_variants=true&yearofbirth=' . $birth_year;
+		return false;
 	}
 
 	static function create_sublink($fullname, $givn, $first, $middle, $prefix, $surn, $surname, $birth_year, $death_year, $gender) {
-		return false;
+		$options = array(
+				'FindMyPast'	=>'http://search.findmypast.com/results/world-records/1939-register?firstname=' . $givn . '&firstname_variants=true&lastname=' . $surn . '&lastname_variants=true&yearofbirth=' . $birth_year,
+				'Ancestry'		=>'https://search.ancestry.co.uk/cgi-bin/sse.dll?db=1939UKRegister&gss=sfs28_ms_db&new=1&rank=1&msT=1&gsfn=' . $givn . '&gsfn_x=0&gsln=' . $surn . '&gsln_x=0&msbdy=' . $birth_year . '&MSAV=1&uidh=lp3',
+			);
+
+		foreach($options as $x=>$x_value) {
+			$link[] = array(
+				'title' => KT_I18N::translate($x),
+				'link'  => $x_value
+			);
+		}
+		return $link;
 	}
 
 	static function createLinkOnly() {
