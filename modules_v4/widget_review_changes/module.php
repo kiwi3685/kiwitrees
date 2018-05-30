3748b1412c4349cd0a9154bc80d9fad2abd09e41
kiwi3685 <Kiwi3685@gmail.com>
2018-05-31 09:39:33 +1200

    Improvements to widget code style

--- modules_v4/widget_review_changes/module.php
+++ modules_v4/widget_review_changes/module.php
@@ -37,7 +37,7 @@ class widget_review_changes_KT_Module extends KT_Module implements KT_Module_Wid
 		return /* I18N: Description of the “Pending changes” module */ KT_I18N::translate('A list of changes that need moderator approval, and email notifications.');
 	}
 
-	// Implement class KT_Module_Block
+	// Implement class KT_Module_Widget
 	public function getWidget($widget_id, $template = true, $cfg = null) {
 		global $KIWITREES_EMAIL;
 		require_once KT_ROOT . 'includes/functions/functions_mail.php';
@@ -62,16 +62,16 @@ class widget_review_changes_KT_Module extends KT_Module implements KT_Module_Wid
 		if ($changes) {
 			//-- if the time difference from the last email is greater than 24 hours then send out another email
 			$LAST_CHANGE_EMAIL = KT_Site::preference('LAST_CHANGE_EMAIL');
-			if (KT_TIMESTAMP - $LAST_CHANGE_EMAIL > (60*60*24*$days)) {
+			if (KT_TIMESTAMP - $LAST_CHANGE_EMAIL > (60 * 60 * 24 * $days)) {
 				$LAST_CHANGE_EMAIL = KT_TIMESTAMP;
 				KT_Site::preference('LAST_CHANGE_EMAIL', $LAST_CHANGE_EMAIL);
 				if ($sendmail == "yes") {
 					// Which users have pending changes?
-					$users_with_changes=array();
+					$users_with_changes = array();
 					foreach (get_all_users() as $user_id=>$user_name) {
 						foreach (KT_Tree::getAll() as $tree) {
 							if (exists_pending_change($user_id, $tree->tree_id)) {
-								$users_with_changes[$user_id]=$user_name;
+								$users_with_changes[$user_id] = $user_name;
 								break;
 							}
 						}
@@ -94,19 +94,19 @@ class widget_review_changes_KT_Module extends KT_Module implements KT_Module_Wid
 				$id		= $this->getName();
 				$class	= $this->getName();
 				if (KT_USER_GEDCOM_ADMIN) {
-					$title = '<i class="icon-admin" title="'.KT_I18N::translate('Configure') . '" onclick="modalDialog(\'block_edit.php?block_id='.$widget_id.'\', \''.$this->getTitle().'\');"></i>';
+					$title = '<i class="icon-admin" title="'.KT_I18N::translate('Configure') . '" onclick="modalDialog(\'block_edit.php?block_id=' . $widget_id . '\', \'' . $this->getTitle() . '\');"></i>';
 				} else {
 					$title = '';
 				}
-				$title .= $this->getTitle().help_link('review_changes', $this->getName());
+				$title .= $this->getTitle() . help_link('review_changes', $this->getName());
 
 				$content = '';
 				if (KT_USER_CAN_ACCEPT) {
 					$content .= '<a href="edit_changes.php" target="_blank" rel="noopener noreferrer">' . KT_I18N::translate('There are pending changes for you to moderate.') . '</a><br>';
 				}
 				if ($sendmail == "yes") {
-					$content .= KT_I18N::translate('Last email reminder was sent ').format_timestamp($LAST_CHANGE_EMAIL)."<br>";
-					$content .= KT_I18N::translate('Next email reminder will be sent after ').format_timestamp($LAST_CHANGE_EMAIL+(60*60*24*$days))."<br><br>";
+					$content .= KT_I18N::translate('Last email reminder was sent ') . format_timestamp($LAST_CHANGE_EMAIL) . "<br>";
+					$content .= KT_I18N::translate('Next email reminder will be sent after ') . format_timestamp($LAST_CHANGE_EMAIL+(60*60*24*$days)) . "<br><br>";
 				}
 				$changes = KT_DB::prepare(
 					"SELECT xref".
@@ -118,21 +118,21 @@ class widget_review_changes_KT_Module extends KT_Module implements KT_Module_Wid
 				foreach ($changes as $change) {
 					$record = KT_GedcomRecord::getInstance($change->xref);
 					if ($record->canDisplayDetails()) {
-						$content.='<b>'.$record->getFullName().'</b>';
+						$content .= '<b>' . $record->getFullName() . '</b>';
 						switch ($record->getType()) {
 						case 'INDI':
 						case 'FAM':
 						case 'SOUR':
 						case 'OBJE':
-							$content.= '<p><a href="'.$record->getHtmlUrl().'">'.KT_I18N::translate('View the changes').'</a></p>';
+							$content .= '<p><a href="'.$record->getHtmlUrl().'">'.KT_I18N::translate('View the changes').'</a></p>';
 							break;
 						}
-						$content.='<br>';
+						$content .= '<br>';
 					}
 				}
 
 				if ($template) {
-					require KT_THEME_DIR.'templates/widget_template.php';
+					require KT_THEME_DIR . 'templates/widget_template.php';
 				} else {
 					return $content;
 				}
@@ -140,7 +140,7 @@ class widget_review_changes_KT_Module extends KT_Module implements KT_Module_Wid
 		}
 	}
 
-	// Implement class KT_Module_Block
+	// Implement class KT_Module_Widget
 	public function loadAjax() {
 		return false;
 	}
@@ -155,7 +155,7 @@ class widget_review_changes_KT_Module extends KT_Module implements KT_Module_Wid
 		return KT_PRIV_USER;
 	}
 
-	// Implement class KT_Module_Block
+	// Implement class KT_Module_Widget
 	public function configureBlock($widget_id) {
 		if (KT_Filter::postBool('save') && KT_Filter::checkCsrf()) {
 			set_block_setting($widget_id, 'days',     KT_Filter::postInteger('num', 1, 180, 7));
@@ -165,14 +165,18 @@ class widget_review_changes_KT_Module extends KT_Module implements KT_Module_Wid
 
 		require_once KT_ROOT.'includes/functions/functions_edit.php';
 
-		$sendmail = get_block_setting($widget_id, 'sendmail', true);
-		$days = get_block_setting($widget_id, 'days', 7);
-		echo '<tr><td class="descriptionbox wrap width33">';
-		echo KT_I18N::translate('Send out reminder emails?');
-		echo '</td><td class="optionbox">';
-		echo edit_field_yes_no('sendmail', $sendmail);
-		echo '<br>';
-		echo KT_I18N::translate('Reminder email frequency (days)')."&nbsp;<input type='text' name='days' value='".$days."' size='2'>";
-		echo '</td></tr>';
-	}
+		$sendmail	= get_block_setting($widget_id, 'sendmail', true);
+		$days		= get_block_setting($widget_id, 'days', 7); ?>
+
+		<tr>
+			<td class="descriptionbox wrap width33">
+				<?php echo KT_I18N::translate('Send out reminder emails?'); ?>
+			</td>
+			<td class="optionbox">
+				<?php echo edit_field_yes_no('sendmail', $sendmail); ?>
+				<br>
+				<?php echo KT_I18N::translate('Reminder email frequency (days)')."&nbsp;<input type='text' name='days' value='".$days."' size='2'>"; ?>
+			</td>
+		</tr>
+	<?php }
 }
