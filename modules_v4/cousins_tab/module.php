f028cadb68af767d782d249ea0ac1551de91a449
kiwi3685 <Kiwi3685@gmail.com>
2018-11-27 15:57:10 +1300

    Implemet optional 2nd cousinns display om Cousins tab

--- modules_v4/cousins_tab/module.php
+++ modules_v4/cousins_tab/module.php
@@ -55,6 +55,17 @@ class cousins_tab_KT_Module extends KT_Module implements KT_Module_Tab {
 	// Implement KT_Module_Tab
 	public function getTabContent() {
 		global $controller;
+
+		$controller->addInlineJavascript('
+			if (jQuery(".secondCousins").is(":visible")) {
+				alert("VISIBLE");
+			} else {
+				alert("OTHER");
+			}
+
+		');
+
+
 		$list_f				= array();
 		$list_f2			= array();
 		$list_f3			= array();
@@ -67,8 +78,15 @@ class cousins_tab_KT_Module extends KT_Module implements KT_Module_Tab {
 		$family				= '';
 		$html				= '';
 		$person				= $controller->getSignificantIndividual();
-		$fullname			=  $controller->record->getFullName();
-		$xref				=  $controller->record->getXref();
+		$fullname			= $controller->record->getFullName();
+		$xref				= $controller->record->getXref();
+
+		?>
+		<script>
+			persistent_toggle("checkbox_second", ".secondCousins");
+		</script>
+		<?php
+
 		if ($person->getPrimaryChildFamily()) {
 			$parentFamily = $person->getPrimaryChildFamily();
 		} else {
@@ -136,82 +154,129 @@ class cousins_tab_KT_Module extends KT_Module implements KT_Module_Tab {
 
 		$myParentFamily = $parentFamily->getXref();
 
-		$html .= '<h3>' . KT_I18N::plural('%2$s has %1$d first cousin recorded', '%2$s has %1$d first cousins recorded', $count_cousins, $count_cousins, $fullname) . '</h3>';
-		if ($count_duplicates > 0) {
-			$html .= '<p>' . /* I18N: a reference to cousins of siblings married to siblings */ KT_I18N::plural('%1$d is on both sides of the family', '%1$d are on both sides of the family', $count_duplicates, $count_duplicates) . '</p>';
-		}
+		$html = '<div class="descriptionbox rela">
+			<input id="checkbox_second" type="checkbox" checked>
+			<label for="checkbox_second">' . KT_I18N::translate('Show second cousins') . '</label>
+		</div>';
+
+
 		$html .= '<div id="cousins_tab_content">';
 
-		//List Cousins (father's family)
-		$html .= '<div id="cousins_f">';
-		$html .= '<h4>'.KT_I18N::translate('Father\'s family (%s)', $count_cousins_f).'</h4>';
-		$i = 0;
-		$prev_fam_id = -1;
-		foreach ($list_f3 as $id3) {
-			$i++;
-			$record=KT_Person::getInstance($id3);
-			$cousinParentFamily = substr($record->getPrimaryChildFamily(), 0, strpos($record->getPrimaryChildFamily(), '@'));
- 			if ( $cousinParentFamily == $myParentFamily )
-				continue; // cannot be cousin to self
-			$family=KT_Family::getInstance($cousinParentFamily);
-			$tmp=array('M'=>'', 'F'=>'F', 'U'=>'NN');
-			$isF=$tmp[$record->getSex()];
-			$label = '';
-			$famcrec = get_sub_record(1, '1 FAMC @'.$cousinParentFamily.'@', $record->getGedcomRecord());
-			$pedi = get_gedcom_value('PEDI', 2, $famcrec, '', false);
-			if ($pedi) {
-				$label = '<span class="cousins_pedi">'.KT_Gedcom_Code_Pedi::getValue($pedi, $record).'</span>';
-			}
-			if ($cousinParentFamily != $prev_fam_id) {
- 				$prev_fam_id = $cousinParentFamily;
-				$html .= '<h5>'.KT_I18N::translate('Parents').'<a target="_blank" rel="noopener noreferrer" href="'. $family->getHtmlUrl(). '">&nbsp;'.$family->getFullName().'</a></h5>';
-				$i = 1;
-			}
-			$html .= '<div class="person_box'.$isF.'">';
-			$html .= '<span class="cousins_counter">'.$i.'</span>';
-			$html .= '<span class="cousins_name"><a target="_blank" rel="noopener noreferrer" href="'. $record->getHtmlUrl(). '">'. $record->getFullName().'</a></span>';
-			$html .= '<span class="cousins_lifespan">'. $record->getLifeSpan(). '</span>';
-			$html .= '<span class="cousins_pedi">'.$label.'</span>';
-			$html .= '</div>';
-		}
-		$html .= '</div>'; // close id="cousins_f"
+			$html.= '<div class="first_cousins" style="display:table; width: 100%;">';
 
-		//List Cousins (mother's family)
-		$prev_fam_id = -1;
-		$html .= '<div id="cousins_m">';
-		$html .= '<h4>'.KT_I18N::translate('Mother\'s family (%s)', $count_cousins_m).'</h4>';
-		$i = 0;
-		foreach ($list_m3 as $id3) {
-			$i++;
-			$record=KT_Person::getInstance($id3);
-			$cousinParentFamily = substr($record->getPrimaryChildFamily(), 0, strpos($record->getPrimaryChildFamily(), '@'));
- 			if ( $cousinParentFamily == $myParentFamily )
- 				continue; // cannot be cousin to self
-			$record=KT_Person::getInstance($id3);
-			$cousinParentFamily = substr($record->getPrimaryChildFamily(), 0, strpos($record->getPrimaryChildFamily(), '@'));
-			$family=KT_Family::getInstance($cousinParentFamily);
-			$tmp=array('M'=>'', 'F'=>'F', 'U'=>'NN');
-			$isF=$tmp[$record->getSex()];
-			$label = '';
-			$famcrec = get_sub_record(1, '1 FAMC @'.$cousinParentFamily.'@', $record->getGedcomRecord());
-			$pedi = get_gedcom_value('PEDI', 2, $famcrec, '', false);
-			if ($pedi) {
-				$label = KT_Gedcom_Code_Pedi::getValue($pedi, $record);
-			}
- 			if ($cousinParentFamily != $prev_fam_id) {
- 				$prev_fam_id = $cousinParentFamily;
-				$html .= '<h5>'.KT_I18N::translate('Parents').'<a target="_blank" rel="noopener noreferrer" href="'. $family->getHtmlUrl(). '">&nbsp;'.$family->getFullName().'</a></h5>';
-				$i = 1;
-			}
-			$html .= '<div class="person_box'.$isF.'">';
-			$html .= '<span class="cousins_counter">'.$i.'</span>';
-			$html .= '<span class="cousins_name"><a target="_blank" rel="noopener noreferrer" href="'. $record->getHtmlUrl(). '">'. $record->getFullName().'</a></span>';
-			$html .= '<span class="cousins_lifespan">'. $record->getLifeSpan(). '</span>';
-			$html .= '<span class="cousins_pedi">'.$label.'</span>';
-			$html .= '</div>';
-		}
-		$html .= '</div>'; // close id="cousins_m"
-		$html .= '</div>'; // close div id="cousins_tab_content"
+				$html .= '
+					<div style="display:table-row;">
+						<h3>' . KT_I18N::plural('%2$s has %1$d first cousin recorded', '%2$s has %1$d first cousins recorded', $count_cousins, $count_cousins, $fullname) . '</h3>';
+					if ($count_duplicates > 0) {
+						$html .= '<p>' . /* I18N: a reference to cousins of siblings married to siblings */ KT_I18N::plural('%1$d is on both sides of the family', '%1$d are on both sides of the family', $count_duplicates, $count_duplicates) . '</p>';
+					}
+					$html .= '</div>
+				';
+				$html .= '<div style="display:table-row;">';
+					//List Cousins (father's family)
+					$html .= '<div class="cousins_f">';
+						$html .= '<h4>'.KT_I18N::translate('Father\'s family (%s)', $count_cousins_f).'</h4>';
+						$i = 0;
+						$prev_fam_id = -1;
+						foreach ($list_f3 as $id3) {
+							$i++;
+							$record = KT_Person::getInstance($id3);
+							$cousinParentFamily = substr($record->getPrimaryChildFamily(), 0, strpos($record->getPrimaryChildFamily(), '@'));
+				 			if ( $cousinParentFamily == $myParentFamily )
+								continue; // cannot be cousin to self
+							$family	= KT_Family::getInstance($cousinParentFamily);
+							$tmp	= array('M'=>'', 'F'=>'F', 'U'=>'NN');
+							$isF	= $tmp[$record->getSex()];
+							$label = '';
+							$famcrec = get_sub_record(1, '1 FAMC @' . $cousinParentFamily . '@', $record->getGedcomRecord());
+							$pedi = get_gedcom_value('PEDI', 2, $famcrec, '', false);
+							if ($pedi) {
+								$label = '<span class="cousins_pedi">' . KT_Gedcom_Code_Pedi::getValue($pedi, $record) . '</span>';
+							}
+							if ($cousinParentFamily != $prev_fam_id) {
+				 				$prev_fam_id = $cousinParentFamily;
+								$html .= '<h5>' . KT_I18N::translate('Parents').'<a target="_blank" rel="noopener noreferrer" href="' . $family->getHtmlUrl() . '">&nbsp;' . $family->getFullName() . '</a></h5>';
+								$i = 1;
+							}
+							$html .= '
+								<div class="person_box' . $isF . '">
+									<span class="cousins_counter">' . $i . '</span>
+									<span class="cousins_name">
+										<a target="_blank" rel="noopener noreferrer" href="' . $record->getHtmlUrl() . '">' . $record->getFullName() . '</a>
+									</span>
+									<span class="cousins_lifespan">' . $record->getLifeSpan() . '</span>
+									<span class="cousins_pedi">' . $label . '</span>
+								</div>
+							';
+						}
+					$html .= '</div>'; // close class="cousins_f"
+					//List Cousins (mother's family)
+					$html .= '<div class="cousins_m">';
+						$html .= '<h4>'.KT_I18N::translate('Mother\'s family (%s)', $count_cousins_m).'</h4>';
+						$i = 0;
+						$prev_fam_id = -1;
+						foreach ($list_m3 as $id3) {
+							$i++;
+							$record = KT_Person::getInstance($id3);
+							$cousinParentFamily = substr($record->getPrimaryChildFamily(), 0, strpos($record->getPrimaryChildFamily(), '@'));
+				 			if ( $cousinParentFamily == $myParentFamily )
+				 				continue; // cannot be cousin to self
+							$record = KT_Person::getInstance($id3);
+							$cousinParentFamily = substr($record->getPrimaryChildFamily(), 0, strpos($record->getPrimaryChildFamily(), '@'));
+							$family = KT_Family::getInstance($cousinParentFamily);
+							$tmp = array('M'=>'', 'F'=>'F', 'U'=>'NN');
+							$isF = $tmp[$record->getSex()];
+							$label = '';
+							$famcrec = get_sub_record(1, '1 FAMC @'.$cousinParentFamily.'@', $record->getGedcomRecord());
+							$pedi = get_gedcom_value('PEDI', 2, $famcrec, '', false);
+							if ($pedi) {
+								$label = KT_Gedcom_Code_Pedi::getValue($pedi, $record);
+							}
+				 			if ($cousinParentFamily != $prev_fam_id) {
+				 				$prev_fam_id = $cousinParentFamily;
+								$html .= '<h5>'.KT_I18N::translate('Parents').'<a target="_blank" rel="noopener noreferrer" href="'. $family->getHtmlUrl(). '">&nbsp;'.$family->getFullName().'</a></h5>';
+								$i = 1;
+							}
+							$html .= '
+								<div class="person_box' . $isF . '">
+									<span class="cousins_counter">' . $i . '</span>
+									<span class="cousins_name">
+										<a target="_blank" rel="noopener noreferrer" href="' . $record->getHtmlUrl() . '">' . $record->getFullName() . '</a>
+									</span>
+									<span class="cousins_lifespan">' . $record->getLifeSpan() . '</span>
+									<span class="cousins_pedi">' . $label . '</span>
+								</div>
+							';
+						}
+					$html .= '</div>'; // close class="cousins_m"
+				$html .= '</div>'; // close "first_cousins"
+			$html .= '</div>'; // close "first_cousins"
+
+			// Second cousins
+			$html .= '<div class="secondCousins">';
+				$html .= '<div class="second_cousins" style="display:table; width: 100%;">';
+					$secondCousinsF = $this->getSecondCousins($grandparentFamilyHusb);
+					$secondCousinsM = $this->getSecondCousins($grandparentFamilyWife);
+					$secondCousins	= $secondCousinsF[1] + $secondCousinsM[1];
+					$html .= '
+						<div style="display:table-row;">
+							<h3>' . KT_I18N::plural('%2$s has %1$d second cousin recorded', '%2$s has %1$d second cousins recorded', $secondCousins, $secondCousins, $fullname) . '</h3>
+						</div>
+						<div style="display:table-row;">
+							<div class="cousins_f">
+								<h4>' . KT_I18N::translate('Second cousins on father\'s side (%s)', $secondCousinsF[1]) . '</h4>'  .
+								$secondCousinsF[0] . '
+							</div>
+							<div class="cousins_m">
+								<h4>' . KT_I18N::translate('Second cousins on mother\'s side (%s)', $secondCousinsM[1]) . '</h4>'  .
+								$secondCousinsM[0] . '
+							</div>
+						</div>
+					</div>
+				';
+			$html .= '</div>'; // close div class="secondCousins"
+			$html .= '</div>'; // close div class="cousins_tab_content"
+
 		return $html;
 
 	}
@@ -229,6 +294,99 @@ class cousins_tab_KT_Module extends KT_Module implements KT_Module_Tab {
 	// Implement KT_Module_Tab
 	public function getPreLoadContent() {
 		return '';
+	}
+
+	function getSecondCousins($grandparentFamily) {
+		$html			= '';
+		$count_2cousins	= 0;
+		$prev_fam_id	= -1;
+		$myGrandFather = $grandparentFamily->getHusband()->getXref();
+		foreach ($grandparentFamily->getHusband()->getPrimaryChildFamily()->getChildren() as $key => $child) {
+			if ($child->getSpouseFamilies() && $child->getXref() <> $myGrandFather) {
+				foreach ($child->getSpouseFamilies() as $family) {
+					if (!is_null($family)) {
+						$i = 0;
+						$children = $family->getChildren();
+						foreach ($children as $key => $child2) {
+							foreach ($child2->getSpouseFamilies() as $family2) {
+								if (!is_null($family2)) {
+									$children2 = $family2->getChildren();
+									foreach ($children2 as $key => $child3) {
+										if ($child->canDisplayName()) {
+											$i ++;
+											$tmp = array('M'=>'', 'F'=>'F', 'U'=>'NN');
+											$isF = $tmp[$child3->getSex()];
+											$cousinParentFamily = substr($child3->getPrimaryChildFamily(), 0, strpos($child3->getPrimaryChildFamily(), '@'));
+											$family = KT_Family::getInstance($cousinParentFamily);
+											if ($cousinParentFamily != $prev_fam_id) {
+								 				$prev_fam_id = $cousinParentFamily;
+												$html .= '<h5>' . KT_I18N::translate('Parents').'<a target="_blank" rel="noopener noreferrer" href="' . $family->getHtmlUrl() . '">&nbsp;' . $family->getFullName() . '</a></h5>';
+												$i = 1;
+											}
+											$html .= '
+												<div class="person_box' . $isF . '">
+													<span class="cousins_counter">' . $i . '</span>
+													<span class="cousins_name">
+														<a target="_blank" rel="noopener noreferrer" href="' . $child3->getHtmlUrl() . '">' . $child3->getFullName() . '</a>
+													</span>
+													<span class="cousins_lifespan">' . $child3->getLifeSpan() . '</span>
+												</div>
+											';
+											$count_2cousins ++;
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+
+		$myGrandMother = $grandparentFamily->getWife()->getXref();
+		foreach ($grandparentFamily->getWife()->getPrimaryChildFamily()->getChildren() as $key => $child) {
+			if ($child->getSpouseFamilies() && $child->getXref() <> $myGrandMother) {
+				foreach ($child->getSpouseFamilies() as $family) {
+					if (!is_null($family)) {
+						$children = $family->getChildren();
+						foreach ($children as $key => $child2) {
+							foreach ($child2->getSpouseFamilies() as $family2) {
+								if (!is_null($family2)) {
+									$i = 0;
+									$children2 = $family2->getChildren();
+									foreach ($children2 as $key => $child3) {
+										if ($child->canDisplayName()) {
+											$i ++;
+											$tmp = array('M'=>'', 'F'=>'F', 'U'=>'NN');
+											$isF = $tmp[$child3->getSex()];
+											$cousinParentFamily = substr($child3->getPrimaryChildFamily(), 0, strpos($child3->getPrimaryChildFamily(), '@'));
+											$family = KT_Family::getInstance($cousinParentFamily);
+											if ($cousinParentFamily != $prev_fam_id) {
+								 				$prev_fam_id = $cousinParentFamily;
+												$html .= '<h5>' . KT_I18N::translate('Parents').'<a target="_blank" rel="noopener noreferrer" href="' . $family->getHtmlUrl() . '">&nbsp;' . $family->getFullName() . '</a></h5>';
+												$i = 1;
+											}
+											$html .= '
+												<div class="person_box' . $isF . '">
+													<span class="cousins_counter">' . $i . '</span>
+													<span class="cousins_name">
+														<a target="_blank" rel="noopener noreferrer" href="' . $child3->getHtmlUrl() . '">' . $child3->getFullName() . '</a>
+													</span>
+													<span class="cousins_lifespan">' . $child3->getLifeSpan() . '</span>
+												</div>
+											';
+											$count_2cousins ++;
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+
+		return array($html, $count_2cousins);
 	}
 
 }
