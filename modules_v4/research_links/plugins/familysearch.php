d074ae67d9f08a9e73fc584d8025e685df7a6813
kiwi3685 <Kiwi3685@gmail.com>
2019-08-20 14:28:45 +1200

    Updated url for Family Search

--- modules_v4/research_links/plugins/familysearch.php
+++ modules_v4/research_links/plugins/familysearch.php
@@ -19,7 +19,7 @@ class familysearch_plugin extends research_base_plugin {
 	}
 
 	static function create_link($fullname, $givn, $first, $middle, $prefix, $surn, $surname, $birth_year, $death_year, $gender) {
-		return $link = 'https://familysearch.org/search/record/results#count=20&query=%2Bgivenname%3A%22' . $givn . '%22~%20%2Bsurname%3A%22' . $surname . '%22~';
+		return $link = 'https://www.familysearch.org/search/record/results?givenname=' . $givn . '&surname=' . $surname . '&count=20';
 	}
 
 	static function create_sublink($fullname, $givn, $first, $middle, $prefix, $surn, $surname, $birth_year, $death_year, $gender) {
