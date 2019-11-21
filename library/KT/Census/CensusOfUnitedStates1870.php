4f50a2f3dd7109d5717df34959b2870615ef5e2b
kiwi3685 <Kiwi3685@gmail.com>
2019-11-21 15:54:46 +1300

    Changes to census templates - US 1870/80

--- library/KT/Census/CensusOfUnitedStates1870.php
+++ library/KT/Census/CensusOfUnitedStates1870.php
@@ -2,13 +2,13 @@
 /**
  * Kiwitrees: Web based Family History software
  * Copyright (C) 2012 to 2018 kiwitrees.net
- * 
+ *
  * Derived from webtrees (www.webtrees.net)
  * Copyright (C) 2010 to 2012 webtrees development team
- * 
+ *
  * Derived from PhpGedView (phpgedview.sourceforge.net)
  * Copyright (C) 2002 to 2010 PGV Development Team
- * 
+ *
  * Kiwitrees is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -41,21 +41,16 @@ class KT_Census_CensusOfUnitedStates1870 extends KT_Census_CensusOfUnitedStates 
 	 */
 	public function columns() {
 		return array(
-			new KT_Census_CensusColumnFullName($this, 'Name', 'Name'),
+			new KT_Census_CensusColumnSurnameGivenNameInitial($this, 'Name', 'Name', 'width: 250px;'),
+			new KT_Census_CensusColumnRelationToHead($this, 'Relation', 'Relation to head of household'),
 			new KT_Census_CensusColumnAge($this, 'Age', 'Age'),
 			new KT_Census_CensusColumnSexMF($this, 'Sex', 'Sex'),
 			new KT_Census_CensusColumnNull($this, 'Color', 'White, Black, Mulatto, Chinese, Indian'),
 			new KT_Census_CensusColumnOccupation($this, 'Occupation', 'Profession, occupation, or trade'),
-			new KT_Census_CensusColumnNull($this, 'RE', 'Value of real estate owned'),
-			new KT_Census_CensusColumnNull($this, 'PE', 'Value of personal estate owned'),
 			new KT_Census_CensusColumnBirthPlaceSimple($this, 'Birthplace', 'Place of birth, naming the state, territory, or country'),
 			new KT_Census_CensusColumnFatherForeign($this, 'FFB', 'Father of foreign birth'),
 			new KT_Census_CensusColumnMotherForeign($this, 'MFB', 'Mother of foreign birth'),
-			new KT_Census_CensusColumnMonthIfBornWithinYear($this, 'Born', 'If born within the year, state month'),
-			new KT_Census_CensusColumnMonthIfMarriedWithinYear($this, 'Mar', 'If married within the year, state month'),
-			new KT_Census_CensusColumnNull($this, 'School', 'Attended school within the year'),
-			new KT_Census_CensusColumnNull($this, 'Read', 'Cannot read'),
-			new KT_Census_CensusColumnNull($this, 'Write', 'Cannot write'),
+			new KT_Census_CensusColumnNull($this, 'R/W', 'Cannot read / write'),
 			new KT_Census_CensusColumnNull($this, 'Infirm', 'Whether deaf and dumb, blind, insane, or idiotic'),
 			new KT_Census_CensusColumnNull($this, 'Cit', 'Male citizen of US'),
 			new KT_Census_CensusColumnNull($this, 'Dis', 'Male citizen of US, where right to vote is denied or abridged'),
--- library/KT/Census/CensusOfUnitedStates1880.php
+++ library/KT/Census/CensusOfUnitedStates1880.php
@@ -2,13 +2,13 @@
 /**
  * Kiwitrees: Web based Family History software
  * Copyright (C) 2012 to 2018 kiwitrees.net
- * 
+ *
  * Derived from webtrees (www.webtrees.net)
  * Copyright (C) 2010 to 2012 webtrees development team
- * 
+ *
  * Derived from PhpGedView (phpgedview.sourceforge.net)
  * Copyright (C) 2002 to 2010 PGV Development Team
- * 
+ *
  * Kiwitrees is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -41,26 +41,15 @@ class KT_Census_CensusOfUnitedStates1880 extends KT_Census_CensusOfUnitedStates 
 	 */
 	public function columns() {
 		return array(
-			new KT_Census_CensusColumnFullName($this, 'Name', 'Name'),
-			new KT_Census_CensusColumnAge($this, 'Age', 'Age'),
+			new KT_Census_CensusColumnSurnameGivenNameInitial($this, 'Name', 'Name', 'width: 250px;'),
+			new KT_Census_CensusColumnNull($this, 'Color', 'White, Black, Mulatto, Chinese, Indian'),
 			new KT_Census_CensusColumnSexMF($this, 'Sex', 'Sex'),
-			new KT_Census_CensusColumnMonthIfBornWithinYear($this, 'Mon', 'If born within the year, state month'),
+			new KT_Census_CensusColumnAge($this, 'Age', 'Age'),
 			new KT_Census_CensusColumnRelationToHead($this, 'Relation', 'Relation to head of household'),
-			new KT_Census_CensusColumnNull($this, 'S', 'Single'),
-			new KT_Census_CensusColumnNull($this, 'M', 'Married'),
-			new KT_Census_CensusColumnNull($this, 'W/D', 'Widowed, Divorced'),
-			new KT_Census_CensusColumnMarriedWithinYear($this, 'MY', 'Married during census year'),
+			new KT_Census_CensusColumnConditionUs($this, 'Cond', 'Whether single, married, widowed, or divorced'),
 			new KT_Census_CensusColumnOccupation($this, 'Occupation', 'Profession, occupation, or trade'),
-			new KT_Census_CensusColumnNull($this, 'UnEm', 'Number of months the person has been unemployed during the census year'),
-			new KT_Census_CensusColumnNull($this, 'Sick', 'Sickness or disability'),
-			new KT_Census_CensusColumnNull($this, 'Blind', 'Blind'),
-			new KT_Census_CensusColumnNull($this, 'DD', 'Deaf and dumb'),
-			new KT_Census_CensusColumnNull($this, 'Idiotic', 'Idiotic'),
-			new KT_Census_CensusColumnNull($this, 'Insane', 'Insane'),
-			new KT_Census_CensusColumnNull($this, 'Disabled', 'Maimed, crippled, bedridden or otherwise disabled'),
-			new KT_Census_CensusColumnNull($this, 'School', 'Attended school within the census year'),
-			new KT_Census_CensusColumnNull($this, 'Read', 'Cannot read'),
-			new KT_Census_CensusColumnNull($this, 'Write', 'Cannot write'),
+			new KT_Census_CensusColumnNull($this, 'Infirm', 'Whether deaf and dumb, blind, insane, or idiotic'),
+			new KT_Census_CensusColumnNull($this, 'R/W', 'Cannot read / write'),
 			new KT_Census_CensusColumnBirthPlaceSimple($this, 'BP', 'Place of birth, naming the state, territory, or country'),
 			new KT_Census_CensusColumnFatherBirthPlaceSimple($this, 'FBP', 'Place of birth of father, naming the state, territory, or country'),
 			new KT_Census_CensusColumnMotherBirthPlaceSimple($this, 'MBP', 'Place of birth of mother, naming the state, territory, or country'),
