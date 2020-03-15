0f7193f9ec7b1dee4d3a59afd147643fa011c214
kiwi3685 <Kiwi3685@gmail.com>
2020-03-16 11:07:47 +1300

    Fix problem of duplicate database entries when re-using old tables.

--- library/KT/Module.php
+++ library/KT/Module.php
@@ -516,9 +516,9 @@ abstract class KT_Module {
 		);
 
 		foreach($default_modules as $module => $order) {
-			KT_DB::prepare(
-				"INSERT INTO `##module` (module_name, status, tab_order, menu_order, sidebar_order, widget_order) VALUES (?, ?, ?, ?, ?, ?)"
-			)->execute(array($module, $order[0], $order[1], $order[2], $order[3], $order[4]));
+			KT_DB::prepare("
+				INSERT IGNORE INTO `##module` (module_name, status, tab_order, menu_order, sidebar_order, widget_order) VALUES (?, ?, ?, ?, ?, ?)
+			")->execute(array($module, $order[0], $order[1], $order[2], $order[3], $order[4]));
 		}
 	}
 
