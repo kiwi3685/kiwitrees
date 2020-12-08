0735e7ab5cf0cba26233852b6fb5b677c5d76913
kiwi3685 <Kiwi3685@gmail.com>
2020-12-08 13:55:47 +1300

    Fis error - ERROR 8: Undefined variable: user_email; 0 Error occurred on line 97 of file message.php

--- message.php
+++ message.php
@@ -94,9 +94,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
             // Check reCAPTCHA response
             if ($responseData->success) {
-                AddToLog('Google reCaptcha valid response from "' . $user_name . '"/"' . $user_email . '", response ="' . $responseData->success . '"', 'auth');
+                AddToLog('Google reCaptcha valid response from "' . $from_name . '"/"' . $from_email . '", response ="' . $responseData->success . '"', 'auth');
             } else {
-                AddToLog('Failed Google reCaptcha response from "' . $user_name . '"/"' . $user_email . '"', 'spam');
+                AddToLog('Failed Google reCaptcha response from "' . $from_name . '"/"' . $from_email . '"', 'spam');
                 KT_FlashMessages::addMessage(KT_I18N::translate('Google reCaptcha robot verification failed, please try again.'));
                 header('Location: ' . KT_SERVER_NAME . KT_SCRIPT_PATH . KT_SCRIPT_NAME);
                 $captcha = false;
