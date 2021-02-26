5dd043bce5f7adfb1aec976e16af09776977ae65
kiwi3685 <Kiwi3685@gmail.com>
2021-02-26 14:28:49 +1300

    Display number of place links a place has on the Googlemaps data page, as hover title over red 'x'.

--- modules_v4/googlemap/admin_places.php
+++ modules_v4/googlemap/admin_places.php
@@ -675,7 +675,7 @@ echo '<div id="gm_tabs">
 									<input type="checkbox" name="del_places[]" class="check" value="'.$place["place_id"].'" title="', KT_I18N::translate('Remove'), '">',
 								'</div></td>';
 							} else {
-								echo '<td><div class="col7"><i class="fa-times"></i></div></td>';
+								echo '<td><div class="col7"><i class="fa-times" title="' . $noRows . '"></i></div></td>';
 							}
 						echo '</tr>';
 					}
