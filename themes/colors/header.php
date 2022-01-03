aec28db9c0131eb26ce700794ca51077bb3489a5
kiwi3685 <kiwi3685@gmail.com>
2022-01-03 15:59:30 +1300

    Update colors theme header

--- themes/colors/header.php
+++ themes/colors/header.php
@@ -35,6 +35,7 @@ $this
 	->addExternalJavascript(KT_JQUERY_AUTOSIZE)
 	->addInlineJavascript('
 		widget_bar();
+		display_help();
 		activate_colorbox();
 		jQuery.extend(jQuery.colorbox.settings, {
 			slideshowStart	:"' . KT_I18N::translate('Play') . '",
@@ -51,7 +52,9 @@ $this
 						}
 			});
 		});
+
 		jQuery("textarea").autosize();
+
 	');
 
 global $ALL_CAPS;
@@ -59,98 +62,100 @@ if ($ALL_CAPS) $this->addInlineJavascript('all_caps();');
 $ctype = safe_REQUEST($_REQUEST, 'ctype', array('gedcom', 'user'), KT_USER_ID ? 'user' : 'gedcom');
 
 $show_widgetbar = false;
+
 if (KT_USER_ID && KT_SCRIPT_NAME != 'index.php' && $view != 'simple' && KT_Module::getActiveWidgets()) {
 	$show_widgetbar = true;
 }
+?>
 
-echo '
 	<!DOCTYPE html>
-	<html ' . KT_I18N::html_markup() . '>
+	<html <?php echo KT_I18N::html_markup(); ?>>
 	<head>
 		<meta charset="UTF-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">' .
-		header_links($META_DESCRIPTION, $META_ROBOTS, $META_GENERATOR, $LINK_CANONICAL) . '
-		<title>' . htmlspecialchars($title) . '</title>
-		<link rel="icon" href="' . KT_THEME_URL . 'images/favicon.png" type="image/png">
-		<link rel="stylesheet" href="' . KT_THEME_URL . 'jquery-ui-custom/jquery-ui.structure.min.css" type="text/css">
-		<link rel="stylesheet" href="' . KT_THEME_URL . 'jquery-ui-custom/jquery-ui.theme.min.css" type="text/css">
-		<link rel="stylesheet" href="' . KT_THEME_URL . 'css/colors.css" type="text/css">
-		<link rel="stylesheet" href="' . KT_THEME_URL, 'css/' . $subColor, '.css" type="text/css">';
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<?php echo header_links($META_DESCRIPTION, $META_ROBOTS, $META_GENERATOR, $LINK_CANONICAL); ?>
+		<title><?php echo htmlspecialchars($title); ?></title>
+		<link rel="icon" href="<?php echo KT_THEME_URL; ?>images/favicon.png" type="image/png">
+		<link rel="stylesheet" href="<?php echo KT_THEME_URL; ?>jquery-ui-custom/jquery-ui.structure.min.css" type="text/css">
+		<link rel="stylesheet" href="<?php echo KT_THEME_URL; ?>jquery-ui-custom/jquery-ui.theme.min.css" type="text/css">
+		<link rel="stylesheet" href="<?php echo KT_THEME_URL; ?>css/colors.css" type="text/css">
+		<link rel="stylesheet" href="<?php echo KT_THEME_URL; ?>css/<?php echo $subColor; ?>.css" type="text/css">
 
-		if (stristr($_SERVER['HTTP_USER_AGENT'], 'iPad')) {
-			echo '<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=0.8, maximum-scale=2.0" />';
-			echo '<link type="text/css" rel="stylesheet" href="' . KT_THEME_URL . 'ipad.css">';
-		} elseif (stristr($_SERVER['HTTP_USER_AGENT'], 'MSIE') || stristr($_SERVER['HTTP_USER_AGENT'], 'Trident')) {
-			// This is needed for all versions of IE, so we cannot use conditional comments.
-			echo '<link type="text/css" rel="stylesheet" href="' . KT_THEME_URL . 'msie.css">';
-		}
-		if (file_exists(KT_THEME_URL . 'mystyle.css')) {
-            echo '<link rel="stylesheet" href="' . KT_THEME_URL . 'mystyle.css' . time() . '" type="text/css">';
-		}
-echo
-	'</head>',
-	'<body id="body">';
+		<?php if (stristr($_SERVER['HTTP_USER_AGENT'], 'iPad')) { ?>
+			<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=0.8, maximum-scale=2.0" />
+			<link type="text/css" rel="stylesheet" href="<?php echo KT_THEME_URL; ?>ipad.css">
+		<?php } elseif (stristr($_SERVER['HTTP_USER_AGENT'], 'MSIE') || stristr($_SERVER['HTTP_USER_AGENT'], 'Trident')) {
+			// This is needed for all versions of IE, so we cannot use conditional comments. ?>
+			<link type="text/css" rel="stylesheet" href="<?php echo KT_THEME_URL; ?>msie.css">
+		<?php }
 
-if ($view!='simple') { // Use "simple" headers for popup windows
-	global $KT_IMAGES;
-	echo '<div id="navbar">
+		if (file_exists(KT_THEME_URL . 'mystyle.css')) { ?>
+			<link rel="stylesheet" href="<?php echo KT_THEME_URL; ?>mystyle.css<?php time(); ?>" type="text/css">
+		<?php } ?>
 
-	<div id="header">' . // Top row left
-		KT_TREE_TITLE . KT_TREE_SUBTITLE;
-		// Top row right
-		echo '<div class="header_search">
-			<form style="display:inline;" action="search.php" method="post">',
-			'<input type="hidden" name="action" value="general">',
-			'<input type="hidden" name="topsearch" value="yes">',
-			'<input type="search" name="query" size="25" placeholder="' . KT_I18N::translate('Search') . '" dir="auto">',
-			'<input class="search-icon" type="image" src="' . $KT_IMAGES['search'] . '" alt="' . KT_I18N::translate('Search') . '" title="' . KT_I18N::translate('Search') . '">',
-			'</form>
+	</head>,
+	<body id="body">
+
+		<?php
+		if ($view!='simple') { // Use "simple" headers for popup windows
+			global $KT_IMAGES; ?>
+			<div id="navbar">
+				<div id="header">
+					<?php echo KT_TREE_TITLE . KT_TREE_SUBTITLE; ?>
+					<div class="header_search">
+						<form style="display:inline;" action="search.php" method="post">
+							<input type="hidden" name="action" value="general">
+							<input type="hidden" name="topsearch" value="yes">
+							<input type="search" name="query" size="25" placeholder="<?php echo KT_I18N::translate('Search'); ?>" dir="auto">
+							<input class="search-icon" type="image" src="<?php echo $KT_IMAGES['search']; ?>" alt="<?php echo KT_I18N::translate('Search'); ?>" title="<?php echo KT_I18N::translate('Search'); ?>">
+						</form>
+					</div>
+					<ul class="makeMenu">
+						<?php if (KT_USER_CAN_ACCEPT && exists_pending_change()) { ?>
+							<li>
+								<a href="edit_changes.php" target="_blank" rel="noopener noreferrer" style="color:red;">
+									<?php echo KT_I18N::translate('Pending changes'); ?>
+								</a>
+							</li>
+						<?php }
+						foreach (KT_MenuBar::getOtherMenus() as $menu) {
+							echo $menu->getMenuAsList();
+						} ?>
+					</ul>
+				</div>
+
+				<!-- Print the main menu bar -->
+				<div id="topMenu">
+					<ul id="main-menu">'
+						<?php if ($show_widgetbar) { ?>
+							<li id="widget-button" class="fa-bars"><a href="#" ><span style="line-height: inherit;" class="fa fa-fw fa-2x fa-bars">&nbsp;</span></a></li>
+						<?php }
+						foreach (KT_MenuBar::getMainMenus() as $menu) {
+							echo getMenuAsCustomList($menu);
+						} ?>
+					</ul>
+					<!-- select menu for responsive layouts only -->
+					<select id="nav-select" onChange="window.location.href=this.value">
+						<option selected="selected" value=""><?php echo KT_I18N::translate('Choose a page'); ?></option>
+						<?php foreach (KT_MenuBar::getMainMenus() as $menu) {
+							echo $menu->getResponsiveMenu();
+						} ?>
+					</select>
+				</div>
+
+			<?php }
+			// Remove list from home when only 1 gedcom
+			$this->addInlineJavaScript(
+				'if (jQuery("#menu-tree ul li").length == 2) jQuery("#menu-tree ul li:last-child").remove();'
+			); ?>
+
 		</div>
-		<ul class="makeMenu">';
-			if (KT_USER_CAN_ACCEPT && exists_pending_change()) {
-				echo '<li>
-					<a href="edit_changes.php" target="_blank" rel="noopener noreferrer" style="color:red;">',
-						KT_I18N::translate('Pending changes') . '
-					</a>
-				</li>';
-			}
-			foreach (KT_MenuBar::getOtherMenus() as $menu) {
-				echo $menu->getMenuAsList();
-			}
-		echo '</ul>
-	</div>';
 
-	// Print the main menu bar
-	echo '<div id="topMenu">
-		<ul id="main-menu">';
-				if ($show_widgetbar) {
-					echo '<li id="widget-button" class="fa-bars"><a href="#" ><span style="line-height: inherit;" class="fa fa-fw fa-2x fa-bars">&nbsp;</span></a></li>';
-				}
-				foreach (KT_MenuBar::getMainMenus() as $menu) {
-					echo getMenuAsCustomList($menu);
-				}
-		echo '</ul>',
-		// select menu for responsive layouts only
-		'<select id="nav-select" onChange="window.location.href=this.value">
-			<option selected="selected" value="">' . KT_I18N::translate('Choose a page') . '</option>';
-			foreach (KT_MenuBar::getMainMenus() as $menu) {
-				echo $menu->getResponsiveMenu();
-			}
-	echo	'</select>
-	</div>';
+		<?php echo $javascript . KT_FlashMessages::getHtmlMessages(); ?>
 
-}
-// Remove list from home when only 1 gedcom
-$this->addInlineJavaScript(
-	'if (jQuery("#menu-tree ul li").length == 2) jQuery("#menu-tree ul li:last-child").remove();'
-);
+	<div id="content">
 
-echo '</div>',// close navbar
-	$javascript .
-	KT_FlashMessages::getHtmlMessages() . // Feedback from asynchronous actions
-	'<div id="content">';
-
-// add widget bar inside content div for all pages except Home, and only for logged in users with role 'member' or above
-if ($show_widgetbar) {
-	include_once 'widget-bar.php';
-}
+	<?php // add widget bar inside content div for all pages except Home, and only for logged in users with role 'member' or above
+	if ($show_widgetbar) {
+		include_once 'widget-bar.php';
+	}
